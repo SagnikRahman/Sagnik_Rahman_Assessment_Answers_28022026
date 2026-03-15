67423eb21ff365693bd7cebe03e3ca5c1f73bd31
SagnikRahman <121563860+SagnikRahman@users.noreply.github.com>
2026-03-15 13:04:26 +0530

    Assessment 14-03-26

---
+++ spring1/src/main/java/com/capgemini/spring1/App.java
@@ -0,0 +1,18 @@
+package com.capgemini.spring1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+    	ApplicationContext context = new ClassPathXmlApplicationContext("beanconfig.xml");
+    	
+    	Student student = (Student)context.getBean("student");
+    	
+    	Employee employee = (Employee)context.getBean("employee");
+    	
+    	System.out.println(student);
+    	System.out.println(employee);
+    	
+    }
+}
---
+++ spring1/src/main/java/com/capgemini/spring1/Student.java
@@ -0,0 +1,53 @@
+package com.capgemini.spring1;
+
+public class Student {
+	private int id;
+	private String name;
+	private String gender;
+	private int marks;
+	
+	public Student() {
+		System.out.println("Student constructor called");
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getMarks() {
+		return marks;
+	}
+
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [id=" + id + 
+		           ", name=" + name + 
+		           ", gender=" + gender + 
+		           ", marks=" + marks + "]";
+	}
+	
+}
