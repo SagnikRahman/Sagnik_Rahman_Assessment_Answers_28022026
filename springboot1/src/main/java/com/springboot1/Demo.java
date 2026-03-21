d3840e25f3bc771dc0b6710c8dc5159c250d107d
SagnikRahman <121563860+SagnikRahman@users.noreply.github.com>
2026-03-21 09:46:18 +0530

    Assignment Spring Boot 1

---
+++ springboot1/src/main/java/com/springboot1/Demo.java
@@ -0,0 +1,13 @@
+package com.springboot1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Demo {
+	
+	@RequestMapping("/")
+	public String show() {
+		return "Hello World";
+	}
+}
